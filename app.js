5298a53031351579d040cef3105a5ea52e133d73
Colin <colin.calnan@gmail.com>
2014-03-08 20:06:33 -0800

    Removing ahoy.js. Moved everything to app.js. Added authentication so only Twilio server from my account can call this number.

--- app.js
+++ app.js
@@ -8,11 +8,48 @@ var app = express();
 app.use(express.urlencoded());
 
 // Create a webhook that handles an incoming SMS
-app.post('/sms', twilio.webhook(), function(request, response) {
+app.post('/sms', twilio.webhook({
+  validate:false
+}), function(request, response) {
     // Create a TwiML response
     var twiml = new twilio.TwimlResponse();
     twiml.message('Hello from node.js!');
     
+    // Render the TwiML response as XML
+    response.send(twiml);
+});
+
+// Create a webhook that handles an incoming SMS
+app.post('/inbound', twilio.webhook(), function(request, response) {
+    // Create a TwiML response
+    var twiml = new twilio.TwimlResponse();
+
+    // Set up a moment
+    var moment = require('moment');
+    m = moment();
+
+    // Set a location
+    var locations = { 
+        'Vancouver' :  '7782324787', 
+        'Montreal'  :  '7789286450',
+    }
+
+    // If it's a weekday
+    if(m.day() <= 6) {
+        // Set everything to UTC as we don't know what server this will be on.
+        m.utc();
+        // Set Zone to Vancuver
+        m.zone("-08:00");
+        // If it's between 5am and 8am in Vancouver then call Montreal, otherwise call Vancouver
+        if(m.hour() >= 5 && m.hour() <= 8) {
+            location = 'Montreal';
+        } else {
+            location = 'Vancouver';
+        }
+    }
+    twiml.say({voice:'alice', language: 'en-CA'}, 'Please wait, we are connecting you to the ' + location + ' office' );
+    twiml.dial(locations[location]);
+
     // Render the TwiML response as XML
     response.send(twiml);
 });
